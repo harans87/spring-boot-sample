8ee4df2b78abfe5f7a120d0310c2777681a45864
Hariharan Subramanian <haran.s87@gmail.com>
2016-04-23 12:26:12 -0400

    Commit spring-boot-sample example files
    Commit spring-boot-sample example files

---
+++ src/main/java/com/sample/spring_boot_sample/Example.java
@@ -0,0 +1,21 @@
+package com.sample.spring_boot_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+	@RequestMapping("/helloworld")
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Example.class, args);
+	}
+
+}
